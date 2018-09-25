8e92b859d63bc069bd059eaf0a886087418b7fb9
genericSpecimen <irfan.sh3112@gmail.com>
2018-09-25 11:28:42 +0530

    updated 11th

--- Data_Structures/Practicals/11.cpp
+++ Data_Structures/Practicals/11.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 #include<string>
-long int fact(unsigned long int num) {
+long long int fact(unsigned long long int num) {
 	if(num < 0) return -1;
 	else if(num == 0) return 1;
 	else return num*fact(num-1);
@@ -18,14 +18,24 @@ void compute_factors(unsigned long int num) {
 	for(int i=1;i<=num/2;i++) {
 		if(num % i == 0) std::cout << i << ",";
 	}
+	std::cout << num << ",";
 	std::cout << std::endl;
 }
 
+void compute_factors_recursive(unsigned long int n, unsigned long int i) {
+	if(i == 0) return;
+	if(n%i == 0) std::cout << i << ", ";
+	return compute_factors_recursive(n,i-1);
+}
 
 int main() {
-	int num=9;
+	unsigned long int num;
+	std::cout << "Enter an integer to calculate its factorial and its factors: ";
+	std::cin >> num;
 	std::cout << "Factorial of " << num  << " is: " << fact(num) << std::endl;
-	std::cout << "Factors of 100: ";
-	compute_factors(100);
+	std::cout << "Factors of " << num << ": ";
+	compute_factors(num);
+	std::cout << "Factors of " << num << " (recursively): ";
+	compute_factors_recursive(num, num);
 	std::cout << std::endl;
 }
