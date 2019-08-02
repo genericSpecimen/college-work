fe4514a9ad9e9dd067e8dec72ab8e9f9ca0ebb7b
Irfan Sheikh <irfan.sh3112@gmail.com>
2019-08-02 17:27:41 +0530

    added DynamicStack

--- IT/Java/StackDriver.java
+++ IT/Java/StackDriver.java
@@ -13,7 +13,7 @@ class StaticStack extends Stack {
 		stack = new int[size];
 		this.size = size;
 		this.top = -1;
-		System.out.println("Initialised stack of size: " + this.size);
+		System.out.println("Initialised static stack of size: " + this.size);
 	}
 
 	void push(int x) {
@@ -25,7 +25,7 @@ class StaticStack extends Stack {
 
 	int pop() {
 		if(top < 0) {
-			System.out.println("Error: Stack underflow!");
+			System.out.printf("%s", "Error: Stack underflow! ");
 			return 0;  
 		} else {
 			int x = stack[top--];
@@ -34,17 +34,50 @@ class StaticStack extends Stack {
 	}
 
 	void display() {
-		System.out.println("\nCurrent Stack is: ");
+		System.out.println("Current stack:");
 		for(int i = top; i >= 0; i--) {
 			if (i == top) System.out.println(stack[i] + " (TOP)");	
 			else System.out.println(stack[i]);
 		}
-		System.out.println("\n");
+		System.out.println();
+	}
+}
+
+class DynamicStack extends Stack {
+	ArrayList<Integer> stack;
+	DynamicStack() {
+		stack = new ArrayList<Integer>();
+	}
+
+	void push(int x) {
+		stack.add(x);
+		System.out.println("=> Pushed " + x);
+	}
+
+	int pop() {
+		if(stack.isEmpty()) {
+			System.out.printf("%s", "Error: Stack underflow! ");
+			return 0;
+		}
+		int top = stack.size() - 1;
+		int popped = stack.get(top);
+		stack.remove(top);
+		return popped;
+	}
+
+	void display() {
+		System.out.println("Current stack:");
+		for(int i = stack.size()-1; i >= 0; i--) {
+			if(i == stack.size()-1) System.out.println(stack.get(i) + " (TOP)");
+			else System.out.println(stack.get(i));
+		}
+		System.out.println();	
 	}
 }
 
 class StackDriver {
 	public static void main(String[] args) {
+		System.out.println("----------Static Stack------------");
 		StaticStack s_stack = new StaticStack(5);
 		s_stack.push(120);
 		s_stack.push(10);
@@ -68,5 +101,29 @@ class StackDriver {
 		System.out.println("=> Popped: " + popped);
 		popped = s_stack.pop();
 		System.out.println("=> Popped: " + popped);
+		System.out.println("----------------------------------\n");
+		
+		System.out.println("----------Dynamic Stack------------");
+		DynamicStack d_stack = new DynamicStack();
+		d_stack.push(10);
+		d_stack.push(20);
+		d_stack.push(30);
+		d_stack.push(40);
+
+		d_stack.display();
+		
+		popped = d_stack.pop();
+		System.out.println("=> Popped: " + popped);
+		popped = d_stack.pop();
+		System.out.println("=> Popped: " + popped);
+		popped = d_stack.pop();
+		System.out.println("=> Popped: " + popped);
+		popped = d_stack.pop();
+		System.out.println("=> Popped: " + popped);
+		popped = d_stack.pop();
+		System.out.println("=> Popped: " + popped);
+		System.out.println("----------------------------------");
+
+
 	}
 }
