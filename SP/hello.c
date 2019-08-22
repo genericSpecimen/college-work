1bb9c9e4e87a8e9a84ee7153db3f6501c70ec0b9
Irfan Sheikh <irfan.sh3112@gmail.com>
2019-08-22 10:25:28 +0530

    modified 4 to ignore function or function calls

--- SP/hello.c
+++ SP/hello.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
-
+void computewithspace          (int v1, int v2) {
+	printf("wow with space\n");
+}
 void compute(int test1, int test2) {
-	printf("wow\n";)
+	printf("wow\n");
 }
 
 int main(void) {
@@ -9,6 +11,8 @@ int main(void) {
 	float b = 20.08;
 	double c = 19.0, d = 0.09;
 	char d, e, f;
+	double longvariablename = 10.01;
+	double __longvariablenamewithunderscores__ = 10.01;
 	printf("Hello world!\n");
 	return 0;
 }
