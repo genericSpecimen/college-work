63e7bd9d2b9e367c959b02dfbd61e1eb82d6490c
genericSpecimen <irfan.sh3112@gmail.com>
2019-12-16 17:27:46 +0530

    test commit

--- system-programming/lex.yy.c
+++ system-programming/lex.yy.c
@@ -354,8 +354,8 @@ static void yynoreturn yy_fatal_error ( const char* msg  );
 	(yy_hold_char) = *yy_cp; \
 	*yy_cp = '\0'; \
 	(yy_c_buf_p) = yy_cp;
-#define YY_NUM_RULES 3
-#define YY_END_OF_BUFFER 4
+#define YY_NUM_RULES 2
+#define YY_END_OF_BUFFER 3
 /* This struct is not used in this scanner,
    but its presence is necessary. */
 struct yy_trans_info
@@ -363,9 +363,9 @@ struct yy_trans_info
 	flex_int32_t yy_verify;
 	flex_int32_t yy_nxt;
 	};
-static const flex_int16_t yy_accept[8] =
+static const flex_int16_t yy_accept[11] =
     {   0,
-        0,    0,    4,    3,    1,    2,    0
+        0,    0,    3,    2,    2,    0,    0,    0,    1,    0
     } ;
 
 static const YY_CHAR yy_ec[256] =
@@ -376,14 +376,14 @@ static const YY_CHAR yy_ec[256] =
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    2,    2,    2,    2,    2,    2,
-        2,    2,    2,    2,    2,    2,    2,    2,    2,    2,
-        2,    2,    2,    2,    2,    2,    2,    2,    2,    2,
-        1,    1,    1,    1,    1,    1,    3,    3,    3,    3,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    2,    1,    1,    1,
 
-        3,    3,    3,    3,    3,    3,    3,    3,    3,    3,
-        3,    3,    3,    3,    3,    3,    3,    3,    3,    3,
-        3,    3,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    3,    1,    1,    1,    1,    1,    4,    1,    1,
+        5,    1,    1,    1,    1,    6,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -400,29 +400,33 @@ static const YY_CHAR yy_ec[256] =
         1,    1,    1,    1,    1
     } ;
 
-static const YY_CHAR yy_meta[4] =
+static const YY_CHAR yy_meta[7] =
     {   0,
-        1,    1,    1
+        1,    2,    1,    3,    4,    5
     } ;
 
-static const flex_int16_t yy_base[8] =
+static const flex_int16_t yy_base[16] =
     {   0,
-        0,    0,    4,    5,    5,    5,    5
+        9,    8,   10,   13,    0,    0,    0,    0,   13,   13,
+        0,    6,    4,    5,    1
     } ;
 
-static const flex_int16_t yy_def[8] =
+static const flex_int16_t yy_def[16] =
     {   0,
-        7,    1,    7,    7,    7,    7,    0
+       11,   11,   10,   10,   12,   13,   14,   15,   10,    0,
+       10,   10,   10,   10,   10
     } ;
 
-static const flex_int16_t yy_nxt[9] =
+static const flex_int16_t yy_nxt[20] =
     {   0,
-        4,    5,    6,    7,    3,    7,    7,    7
+        4,    4,    4,    4,    4,    9,    8,    7,    6,   10,
+        5,    5,    3,   10,   10,   10,   10,   10,   10
     } ;
 
-static const flex_int16_t yy_chk[9] =
+static const flex_int16_t yy_chk[20] =
     {   0,
-        1,    1,    1,    3,    7,    7,    7,    7
+       11,   11,   11,   11,   11,   15,   14,   13,   12,    3,
+        2,    1,   10,   10,   10,   10,   10,   10,   10
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -439,11 +443,11 @@ int yy_flex_debug = 0;
 #define YY_MORE_ADJ 0
 #define YY_RESTORE_YY_MORE_OFFSET
 char *yytext;
-#line 1 "2_caesar-cipher.l"
-#line 2 "2_caesar-cipher.l"
+#line 1 "3.5.3.l"
+#line 2 "3.5.3.l"
 	#include <stdio.h>
-#line 445 "lex.yy.c"
-#line 446 "lex.yy.c"
+#line 449 "lex.yy.c"
+#line 450 "lex.yy.c"
 
 #define INITIAL 0
 
@@ -660,9 +664,10 @@ YY_DECL
 		}
 
 	{
-#line 7 "2_caesar-cipher.l"
+#line 7 "3.5.3.l"
 
-#line 665 "lex.yy.c"
+
+#line 670 "lex.yy.c"
 
 	while ( /*CONSTCOND*/1 )		/* loops until end-of-file is reached */
 		{
@@ -689,13 +694,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 8 )
+				if ( yy_current_state >= 11 )
 					yy_c = yy_meta[yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 5 );
+		while ( yy_base[yy_current_state] != 13 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -721,28 +726,15 @@ do_action:	/* This label is used only to access EOF actions. */
 
 case 1:
 YY_RULE_SETUP
-#line 8 "2_caesar-cipher.l"
-{
-		char ch = yytext[0];
-		ch = ((ch - 'A' + 3) % 26) + 'A';
-		printf ("%c", ch);
-	}
+#line 9 "3.5.3.l"
+{ fprintf(yyout, "double"); }
 	YY_BREAK
 case 2:
 YY_RULE_SETUP
-#line 13 "2_caesar-cipher.l"
-{
-		char ch = yytext[0];
-		ch = ((ch - 'a' + 3) % 26) + 'a';
-		printf ("%c", ch);
-	}
-	YY_BREAK
-case 3:
-YY_RULE_SETUP
-#line 20 "2_caesar-cipher.l"
+#line 11 "3.5.3.l"
 ECHO;
 	YY_BREAK
-#line 745 "lex.yy.c"
+#line 737 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -1039,7 +1031,7 @@ static int yy_get_next_buffer (void)
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 8 )
+			if ( yy_current_state >= 11 )
 				yy_c = yy_meta[yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
@@ -1067,11 +1059,11 @@ static int yy_get_next_buffer (void)
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 8 )
+		if ( yy_current_state >= 11 )
 			yy_c = yy_meta[yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
-	yy_is_jam = (yy_current_state == 7);
+	yy_is_jam = (yy_current_state == 10);
 
 		return yy_is_jam ? 0 : yy_current_state;
 }
@@ -1747,12 +1739,13 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 20 "2_caesar-cipher.l"
+#line 11 "3.5.3.l"
 
 
-int main(void) {
+int main() {
+	yyin = fopen("input.txt", "r");
+	yyout = fopen("output.txt", "w");
 	yylex();
-	return 0;
 }
 
 
