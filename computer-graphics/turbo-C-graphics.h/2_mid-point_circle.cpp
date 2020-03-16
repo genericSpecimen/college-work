bd0d08fb8e9d572a949d07993d58a372fc94ccfd
genericSpecimen <irfan.sh3112@gmail.com>
2020-03-16 15:22:55 +0530

    handle negative values in circle drawing by ignoring them

--- computer-graphics/turbo-C-graphics.h/2_mid-point_circle.cpp
+++ computer-graphics/turbo-C-graphics.h/2_mid-point_circle.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <graphics.h>
 
+/*
 void symmetric_circle_points(int cx, int cy, int px, int py) {
 	putpixel(cx + px, cy + py, GREEN);
 	putpixel(cx + px, cy - py, GREEN);
@@ -12,6 +13,23 @@ void symmetric_circle_points(int cx, int cy, int px, int py) {
 	putpixel(cx - py, cy + px, GREEN);
 	putpixel(cx - py, cy - px, GREEN);
 }
+*/
+
+void symmetric_circle_points(int cx, int cy, int px, int py) {
+	int sign[2] = {-1, 1};
+	int x, y, x1, y1;
+	for(int i=0;i<4;i++) {
+		x = cx + sign[i/2%2] * px;
+		y = cy + sign[i%2] * py;
+
+		x1 = cx + sign[i/2%2] * py;
+		y1 = cy + sign[i%2] * px;
+		// std::cout << "cx " << sign[i/2%2]  << " px" << ", cy " << sign[i%2] << " py"<< "\n";
+		if(!(x < 0 || y < 0)) putpixel(x, y, GREEN);
+		if(!(x1 < 0 || y1 < 0)) putpixel(x1, y1, GREEN);
+	}
+}
+
 
 void mid_point_circle(int cx, int cy, int radius) {
 	int x = 0, y = radius;
@@ -39,7 +57,7 @@ int main() {
 	int gd = DETECT, gm;
 	initgraph(&gd, &gm, NULL);
 
-	mid_point_circle(250, 250, 150);
+	mid_point_circle(0, 0, 100);
 	
 	getch();
 	closegraph();
